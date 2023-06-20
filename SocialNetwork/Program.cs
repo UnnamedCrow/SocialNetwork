e36599975c305428e8923bd045e279aa99a4d6f8
Максим Самисько <masechk@gmail.com>
2023-06-20 16:34:06 +0300

    SocialNetwork - Homework: Added UserServise with func Registration

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace SocialNetwork
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+
+namespace SocialNetwork
 {
     internal class Program
     {
+        public static UserSevice userSevice = new UserSevice();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать в социальную сеть");
+
+            while (true)
+            {
+
+
+
+                Console.Write("Для регистрации пользователя введите имя пользователя:");
+                string firstName = Console.ReadLine();
+
+                Console.Write("фамилия: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("пароль: ");
+                string password = Console.ReadLine();
+
+                Console.Write("почтовый адрес: ");
+                string email = Console.ReadLine();
+
+                UserRegistrationData userRegistrationData = new UserRegistrationData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userSevice.Register(userRegistrationData);
+                    Console.WriteLine("Регистрация прошла успешно!");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Введите корректные данные");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Произошла ошибка регистрации");
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
